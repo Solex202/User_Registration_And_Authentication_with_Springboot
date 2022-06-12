dbcc317c1ff3dd2215d35dea153a578b494c717b
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-06-12 17:02:42 +0100

    update

---
+++ src/main/java/com/shootfish/xyz/task/userRegistrationAndAuthentication/exceptions/EmailValidationException.java
@@ -0,0 +1,8 @@
+package com.shootfish.xyz.task.userRegistrationAndAuthentication.exceptions;
+
+public class EmailValidationException extends RuntimeException{
+
+    public EmailValidationException(String message) {
+        super(message);
+    }
+}
