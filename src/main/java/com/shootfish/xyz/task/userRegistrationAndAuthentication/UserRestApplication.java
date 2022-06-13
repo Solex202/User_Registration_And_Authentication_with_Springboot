1949a166f3199ddecf9a3a9885deef0bda7a4bb9
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-06-13 08:51:28 +0100

    change

--- src/main/java/com/shootfish/xyz/task/userRegistrationAndAuthentication/UserRestApplication.java
+++ src/main/java/com/shootfish/xyz/task/userRegistrationAndAuthentication/UserRestApplication.java
@@ -2,7 +2,10 @@ package com.shootfish.xyz.task.userRegistrationAndAuthentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan("com.shootfish.xyz.task.userRegistrationAndAuthentication")
@@ -10,6 +13,18 @@ public class UserRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserRestApplication.class, args);
+
+
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins("https://user-registration-app-frontend.herokuapp.com/");
+				registry.addMapping("/**").allowedOrigins("https://user-registration-app-frontend.herokuapp.com/");
+			}
+		};
 	}
 
 }
